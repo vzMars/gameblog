c61a2ba9ce504030347d14425f602ea2366508f1
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-09 11:17:08 -0400

    init components that will be used for public and private routes

---
+++ src/components/RequireAuth.tsx
@@ -0,0 +1,9 @@
+const RequireAuth = () => {
+  return (
+    <div>
+      <h1>RequireAuth</h1>
+    </div>
+  );
+};
+
+export default RequireAuth;
---
+++ src/components/RequireGuest.tsx
@@ -0,0 +1,9 @@
+const RequireGuest = () => {
+  return (
+    <div>
+      <h1>RequireGuest</h1>
+    </div>
+  );
+};
+
+export default RequireGuest;
