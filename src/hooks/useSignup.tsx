3ca944a28f8a8f49c2a9528404d4cd7f3d40bba1
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-10 17:46:26 -0400

    custom hook that lets a user sign up for the app and then logs them in

---
+++ src/hooks/useSignup.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+import { UserType } from '../context/AuthContext';
+
+export const useSignup = () => {
+  const [error, setError] = useState<boolean | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean | null>(null);
+  const { dispatch } = useAuthContext();
+
+  const signup = async (email: string, username: string, password: string) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch('http://localhost:5000/api/auth/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ email, username, password }),
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      const user: UserType = json.user;
+      dispatch({ type: 'LOGIN', payload: { user: user, isLoading: false } });
+      setIsLoading(false);
+    }
+  };
+
+  return { signup, isLoading, error };
+};
+
+export default useSignup;
