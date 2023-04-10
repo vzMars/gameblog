4ad13e48463743215b9b1ce34d2419b6ed9ba394
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-10 17:18:08 -0400

    header component styling for desktop and mobile and using hamburger-react animated menu icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+import Hamburger from 'hamburger-react';
+import Nav from './Nav';
+import MobileNav from './MobileNav';
+
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const { user } = useAuthContext().state;
+
   return (
-    <header className='text-white'>
-      <h1>Header</h1>
+    <header className='fixed top-0 w-full bg-neutral-900 font-lexbold text-white'>
+      <section className='mx-auto flex max-w-6xl items-center justify-between p-4'>
+        <Link
+          to='/'
+          className='text-3xl hover:text-sky-800'
+          onClick={() => setOpen(false)}
+        >
+          GameBlog
+        </Link>
+        <div className='lg:hidden'>
+          <Hamburger toggled={open} toggle={setOpen} rounded size={32} />
+        </div>
+        <Nav user={user} />
+      </section>
+      <MobileNav user={user} open={open} setOpen={setOpen} />
     </header>
   );
 };
