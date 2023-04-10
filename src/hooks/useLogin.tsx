f451871bc4540a4fd977eca8c79bd9f5eba06826
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-10 17:40:37 -0400

    custom hook for logging in the user

---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+import { UserType } from '../context/AuthContext';
+
+export const useLogin = () => {
+  const [error, setError] = useState<boolean | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean | null>(null);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email: string, password: string) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch('http://localhost:5000/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ email, password }),
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+
+    if (response.ok) {
+      const user: UserType = json.user;
+      dispatch({ type: 'LOGIN', payload: { user: user, isLoading: false } });
+      setIsLoading(false);
+    }
+  };
+
+  return { login, isLoading, error };
+};
