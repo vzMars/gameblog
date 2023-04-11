70ddf9df8671f8030f35301e9d98c475696041cf
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-10 22:20:17 -0400

    login page styling added and user is able to login with their email and password using the useLogin hook

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,49 @@
+import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
+import Error from '../components/Error';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, error, isLoading } = useLogin();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await login(email, password);
+  };
+
   return (
-    <main className='text-white'>
-      <h1>Login</h1>
+    <main className='font-lexreg text-white'>
+      <section className='mx-auto max-w-6xl px-4 pb-12 pt-32 md:pt-40'>
+        <form
+          className='mx-auto flex max-w-sm flex-col rounded-md bg-neutral-900 p-4 py-12'
+          onSubmit={handleSubmit}
+        >
+          <h1 className='mb-6 font-lexbold text-4xl'>Login</h1>
+          <label className='mb-1'>Email</label>
+          <input
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className='mb-6 rounded-md p-2 text-black'
+          />
+          <label className='mb-1'>Password</label>
+          <input
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            className='mb-7 rounded-md p-2 text-black'
+          />
+          <button
+            disabled={isLoading}
+            className='mb-6 w-full rounded-md bg-sky-800 px-2.5 py-2 font-lexbold hover:bg-sky-900'
+          >
+            Sign Up
+          </button>
+          {error && <Error errorMessage={error} />}
+        </form>
+      </section>
     </main>
   );
 };
