842bf5625f09294c9ed9b2f6841703fc1a743279
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-11 13:38:08 -0400

    footer component with link to my github profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,14 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <footer className='text-white'>
-      <h1>Footer</h1>
+    <footer className='mt-auto font-kgcs text-white'>
+      <section className='mx-auto flex max-w-6xl justify-center px-4 pb-12 pt-32 md:pt-40'>
+        <p>
+          &copy; {currentYear}{' '}
+          <a href='https://github.com/vzMars'>Marcos Gonzalez</a>
+        </p>
+      </section>
     </footer>
   );
 };
