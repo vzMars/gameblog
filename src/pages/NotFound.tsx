5f12426cfcd31c1b91af487e99fe8682e967a70f
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-11 14:39:32 -0400

    not found page styling with link back to home page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
   return (
-    <main className='text-white'>
-      <h1>NotFound</h1>
+    <main className='m-auto font-lexbold text-white'>
+      <section className='mx-auto flex max-w-6xl flex-col items-center justify-center space-y-8 px-4 pt-32 md:pt-40'>
+        <h1 className='text-9xl font-bold text-sky-800'>404</h1>
+        <p className='lead fw-normal text-center font-medium'>
+          Oops! The page you are looking for does not exist.
+        </p>
+        <Link
+          to='/'
+          className='rounded-md bg-sky-800 px-4 py-3 text-xl font-bold hover:bg-sky-900'
+        >
+          Back To Home
+        </Link>
+      </section>
     </main>
   );
 };
