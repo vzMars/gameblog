0529cd579611c351596b89c581335bfc809e681a
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-10 22:28:02 -0400

    added routes to login, signup pages which are wrapped inside of RequireGuest component, added route to profile page which is wrapped inside of RequireAuth component and route to a not found 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// components
+import RequireAuth from './components/RequireAuth';
+import RequireGuest from './components/RequireGuest';
 
 // pages
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
 
 // layouts
-import Layout from './Layouts/Layout';
+import Layout from './layouts/Layout';
 
 const App = () => {
   return (
@@ -12,6 +20,18 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
+
+          <Route element={<RequireGuest />}>
+            <Route path='login' element={<Login />} />
+            <Route path='signup' element={<Signup />} />
+          </Route>
+
+          <Route element={<RequireAuth />}>
+            <Route path='profile' element={<Profile />} />
+          </Route>
+
+          <Route path='404' element={<NotFound />} />
+          <Route path='*' element={<Navigate to='404' replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
