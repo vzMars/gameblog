ddce0f4ee83a4180c2c7829631c8cab6c3221ec5
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-15 11:38:57 -0400

    PostContext created

---
+++ src/context/PostContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, useReducer, useEffect, ReactElement } from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { getPosts } from '../services/getPosts';
+import {
+  PostStateType,
+  PostContextType,
+  PostReducerAction,
+  ChildrenType,
+} from '../types';
+
+const initialState: PostStateType = {
+  posts: [],
+};
+
+const initPostContextState: PostContextType = {
+  posts: initialState.posts,
+  dispatch: () => {},
+};
+
+export const PostContext = createContext<PostContextType>(initPostContextState);
+
+export const postReducer = (
+  state: PostStateType,
+  action: PostReducerAction
+): PostStateType => {
+  switch (action.type) {
+    case 'SET':
+      if (!action.payload) {
+        throw new Error('action.payload missing in SET action');
+      }
+
+      return {
+        posts: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const PostProvider = ({ children }: ChildrenType): ReactElement => {
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    if (user) {
+      getPosts(dispatch);
+    }
+  }, []);
+
+  return (
+    <PostContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </PostContext.Provider>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
+import { PostProvider } from './context/PostContext';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <PostProvider>
+        <App />
+      </PostProvider>
     </AuthProvider>
   </React.StrictMode>
 );
