029ecd0ffed1077c0581e0230cd40358e34d3ff6
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-15 11:29:13 -0400

    types folder created

---
+++ src/types/index.ts
@@ -0,0 +1,57 @@
+import { ReactElement } from 'react';
+
+export type ChildrenType = { children?: ReactElement | ReactElement[] };
+
+// AuthContext
+export type UserType = {
+  id: string;
+  email: string;
+  username: string;
+};
+
+export type AuthStateType = {
+  user: UserType | null;
+  isLoading: boolean;
+};
+
+export type AuthContextType = {
+  user: UserType | null;
+  isLoading: boolean;
+  dispatch: React.Dispatch<AuthReducerAction>;
+};
+
+export type AuthReducerAction =
+  | { type: 'LOGIN'; payload: UserType }
+  | { type: 'LOGOUT' };
+
+// PostContext
+export type PostUserType = {
+  _id: string;
+  username: string;
+};
+
+export type PostType = {
+  _id: string;
+  title: string;
+  content: string;
+  image: string;
+  cloudinaryId: string;
+  tag: string;
+  user: PostUserType;
+  createdAt: string;
+};
+
+export type PostStateType = {
+  posts: PostType[];
+};
+
+export type PostContextType = {
+  posts: PostType[];
+  dispatch: React.Dispatch<PostReducerAction>;
+};
+
+export type PostReducerAction =
+  | { type: 'SET'; payload: PostType[] }
+  | { type: 'CREATE'; payload: PostType }
+  | { type: 'UPDATE'; payload: PostType }
+  | { type: 'DELETE'; payload: PostType };
