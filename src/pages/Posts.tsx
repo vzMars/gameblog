2b1ea6af569908494f050649a0e31924f7405312
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-15 19:29:33 -0400

    Latest posts page that returns a list of all the posts sorted by newest

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,11 +1,20 @@
-const LatestNews = () => {
+import { usePostContext } from '../hooks/usePostContext';
+import PostCard from '../components/PostCard';
+
+const Posts = () => {
+  const { posts } = usePostContext();
   return (
     <main className='font-lexreg text-white'>
-      <section className='mx-auto max-w-6xl px-4 pt-32 md:pt-40'>
-        <h1 className='font-lexbold text-3xl'>Latest News</h1>
+      <section className='mx-auto max-w-6xl px-4 pt-32'>
+        <h1 className='mb-3 font-lexbold text-3xl'>Latest News</h1>
+        <ul>
+          {posts.map((post) => (
+            <PostCard post={post} key={post._id} />
+          ))}
+        </ul>
       </section>
     </main>
   );
 };
 
-export default LatestNews;
+export default Posts;
