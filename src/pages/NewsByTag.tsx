1e16e30ffd27e9e1f5d5b050b015ebd0ba42b3dd
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-16 18:03:51 -0400

    page that renders a list of posts filtered by their tag

--- src/pages/NewsByTag.tsx
+++ src/pages/NewsByTag.tsx
@@ -1,12 +1,66 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { usePostContext } from '../hooks/usePostContext';
+import PostCard from '../components/PostCard';
+import Loader from '../components/Loader';
 
 const NewsByTag = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tagName, setTagName] = useState('');
   const { tag } = useParams();
+  const { posts } = usePostContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getTag = async () => {
+      setIsLoading(true);
+
+      const response = await fetch(
+        `http://localhost:5000/api/posts/tag/${tag}`,
+        {
+          method: 'GET',
+          credentials: 'include',
+        }
+      );
+
+      const json = await response.json();
+
+      if (!response.ok) {
+        setIsLoading(false);
+        navigate('/404');
+      }
+
+      if (response.ok) {
+        setTagName(json);
+        setIsLoading(false);
+      }
+    };
+
+    getTag();
+  }, []);
+
+  const tagPosts = posts.filter((post) => post.tag === tagName);
 
   return (
     <main className='font-lexreg text-white'>
       <section className='mx-auto max-w-6xl px-4 pt-32'>
-        <h1 className='mb-3 font-lexbold text-3xl'>{tag}</h1>
+        {isLoading ? (
+          <div className='text-center'>
+            <Loader />
+          </div>
+        ) : (
+          <>
+            <h1 className='mb-3 font-lexbold text-3xl'>
+              {tagName.toUpperCase().slice(0, 1) + tagName.slice(1)}
+            </h1>
+            <ul>
+              {tagPosts.map((post) => (
+                <PostCard post={post} key={post._id} />
+              ))}
+            </ul>
+          </>
+        )}
+
         <ul></ul>
       </section>
     </main>
