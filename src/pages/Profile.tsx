7c98d18cfeac44cdae2f579a854e79e4386aa7d1
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-16 16:24:30 -0400

    profile page styling and check if user profile actually exists before rendering the content, if not navigate to 404 page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,65 @@
+import { useParams, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { usePostContext } from '../hooks/usePostContext';
+import PostCard from '../components/PostCard';
+import Loader from '../components/Loader';
+
 const Profile = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [profileName, setProfileName] = useState('');
+  const { username } = useParams();
+  const { posts } = usePostContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getProfile = async () => {
+      setIsLoading(true);
+
+      const response = await fetch(
+        `http://localhost:5000/api/user/${username}`,
+        {
+          method: 'GET',
+          credentials: 'include',
+        }
+      );
+
+      const json = await response.json();
+
+      if (!response.ok) {
+        setIsLoading(false);
+        navigate('/404');
+      }
+
+      if (response.ok) {
+        setProfileName(json);
+        setIsLoading(false);
+      }
+    };
+
+    getProfile();
+  }, []);
+
+  const profilePosts = posts.filter(
+    (post) => post.user.username === profileName
+  );
+
   return (
     <main className='font-lexreg text-white'>
-      <section className='mx-auto max-w-6xl px-4 pt-32 md:pt-40'>
-        <h1 className='font-lexbold text-3xl'>Profile</h1>
+      <section className='mx-auto max-w-6xl px-4 pt-32'>
+        {isLoading ? (
+          <div className='text-center'>
+            <Loader />
+          </div>
+        ) : (
+          <>
+            <h1 className='mb-3 font-lexbold text-3xl'>{profileName}</h1>
+            <ul>
+              {profilePosts.map((post) => (
+                <PostCard post={post} key={post._id} />
+              ))}
+            </ul>
+          </>
+        )}
       </section>
     </main>
   );
