575cd3fcd799f9f24e6b551ad8a4649e2e6243af
Marcos Gonzalez <marcosgonzalez1731@gmail.com>
2023-04-17 11:09:41 -0400

    PostDetails page styling, checks for valid post before rendering and users can delete their posts

---
+++ src/components/PostDetailsCard.tsx
@@ -0,0 +1,78 @@
+import { PostType } from '../types';
+import { formatDistanceToNow } from 'date-fns';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { usePostContext } from '../hooks/usePostContext';
+import { FaTrash, FaEdit } from 'react-icons/fa';
+
+type PropsType = {
+  post: PostType;
+};
+
+const PostDetailsCard = ({ post }: PropsType) => {
+  const { user } = useAuthContext();
+  const { dispatch } = usePostContext();
+  const navigate = useNavigate();
+
+  const deletePost = async (id: string) => {
+    const response = await fetch(`http://localhost:5000/api/posts/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: 'DELETE', payload: json });
+      navigate('/news');
+    }
+  };
+
+  return (
+    <div className='mx-auto mb-4 max-w-3xl overflow-hidden rounded-md border border-neutral-800 bg-neutral-900'>
+      <img src={post.image} alt={post.title} className='w-full object-cover' />
+      <div className='flex flex-col p-2'>
+        <h2 className='font-lexbold text-2xl hover:text-sky-800'>
+          {post.title}
+        </h2>
+        <div className='flex flex-col'>
+          <p>
+            By{' '}
+            <Link to={`/user/${post.user.username}`}>{post.user.username}</Link>
+          </p>
+          <span className='text-sm text-neutral-500'>
+            {formatDistanceToNow(new Date(post.createdAt), {
+              addSuffix: true,
+            })}
+          </span>
+        </div>
+        <p className='my-4 whitespace-pre-line text-neutral-200'>
+          {post.content}
+        </p>
+        <div className='flex items-center justify-between'>
+          <Link to={`/tag/${post.tag}`} className='pb-2'>
+            <span className='mb-4 rounded-sm bg-sky-800 p-2 hover:bg-sky-900'>
+              {post.tag}
+            </span>
+          </Link>
+          {user && user.id === post.user._id && (
+            <div className='mb-6 flex space-x-2'>
+              <Link to={`/edit/${post._id}`}>
+                <FaEdit className='cursor-pointer self-start text-2xl text-sky-800 hover:text-sky-900 md:text-3xl' />
+              </Link>
+              <FaTrash
+                className='cursor-pointer self-start text-2xl text-sky-800 hover:text-sky-900 md:text-3xl'
+                onClick={() => deletePost(post._id)}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDetailsCard;
--- src/pages/PostDetails.tsx
+++ src/pages/PostDetails.tsx
@@ -1,10 +1,54 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { PostType } from '../types';
+import PostDetailsCard from '../components/PostDetailsCard';
+import Loader from '../components/Loader';
 
 const PostDetails = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState<PostType | null>(null);
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkPostId = async () => {
+      setIsLoading(true);
+
+      const response = await fetch(`http://localhost:5000/api/posts/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+
+      const json = await response.json();
+
+      if (!response.ok) {
+        setIsLoading(false);
+        navigate('/404');
+      }
+
+      if (response.ok) {
+        setPost(json);
+        setIsLoading(false);
+      }
+    };
+
+    checkPostId();
+  }, []);
+
   return (
     <main className='font-lexreg text-white'>
-      <section className='mx-auto max-w-6xl px-4 pt-32'>{id}</section>
+      <section className='mx-auto max-w-6xl px-4 pt-32'>
+        {!isLoading && post ? (
+          <PostDetailsCard post={post} />
+        ) : (
+          <div className='text-center'>
+            <Loader />
+          </div>
+        )}
+      </section>
     </main>
   );
 };
